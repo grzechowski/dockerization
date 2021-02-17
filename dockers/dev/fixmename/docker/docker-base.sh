706eca57ea59fec5b3e0d5b6852510c510c7ab86
Tomasz Grzechowski <keo@becomewater.com>
2021-02-17 16:15:39 +0100

    fix scripts and missing compose

--- dockers/dev/fixmename/docker/docker-base.sh
+++ dockers/dev/fixmename/docker/docker-base.sh
@@ -10,12 +10,12 @@ do
 done
 
 # test database start
-until [ "$(echo "show databases like 'fixmename'" | mysql -uroot -h172.16.01.104 -pfixmename -N 2>/dev/null | wc -l)" -gt 0 ]
+until [ "$(echo "show databases like 'fixmename'" | mysql -uroot -h172.16.1.104 -pfixmename -N 2>/dev/null | wc -l)" -gt 0 ]
 do
   echo "Waiting for database server to start"
   sleep 1
 done
 
 # fix storage
-chmod -R a+rwx storage
-chmod -R a+rwx boostrap/cache
+for i in `find storage -type d`; do chmod a+rwx $i; done
+chmod -R a+rwx bootstrap/cache
